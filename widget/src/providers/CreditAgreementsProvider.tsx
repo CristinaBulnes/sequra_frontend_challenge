39f813e4daf13df38f91db5042f9490ddc0c8fae
Cristina Bulnes <c.bulnes@stuart.com>
2024-04-29 09:23:46 +0200

    fix: reset option selected on price change

--- widget/src/providers/CreditAgreementsProvider.tsx
+++ widget/src/providers/CreditAgreementsProvider.tsx
@@ -1,13 +1,30 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useEffect, useMemo, useState } from "react";
 
 import useCreditAgreements, {
   CreditAgreement,
 } from "../hooks/useCreditAgreements";
+import { SelectOption } from "../components/Select/Select";
+
+function getProductPrice(productPriceElement: HTMLElement) {
+  const productPriceStringValue = productPriceElement.textContent?.replace(
+    ",",
+    "."
+  );
+
+  const productPrice =
+    productPriceStringValue && parseFloat(productPriceStringValue);
+
+  const productPriceInEurCents = productPrice && productPrice * 100;
+
+  return productPriceInEurCents;
+}
 
 export interface CreditAgreementsContextType {
   creditAgreements: CreditAgreement[];
   instalmentFee: string;
   setPrice: React.Dispatch<React.SetStateAction<number>>;
+  creditSelected: SelectOption | null;
+  setCreditSelected: React.Dispatch<React.SetStateAction<SelectOption | null>>;
 }
 
 export const CreditAgreementsContext =
@@ -19,6 +36,34 @@ type Props = {
 
 const CreditAgreementsProvider = ({ children }: Props) => {
   const [price, setPrice] = useState<number>(0);
+  const [creditSelected, setCreditSelected] = useState<SelectOption | null>(
+    null
+  );
+
+  const updatePrice = () => {
+    const productPriceElement = document.getElementById("product-price");
+
+    const price = productPriceElement && getProductPrice(productPriceElement);
+
+    if (price) {
+      setPrice(price);
+    }
+
+    // On price updated, reset the selected option value
+    setCreditSelected(null);
+  };
+
+  useEffect(() => {
+    Array.from(document.getElementsByClassName("product-capacity")).map(
+      (element) => element.addEventListener("click", updatePrice)
+    );
+
+    return () => {
+      Array.from(document.getElementsByClassName("product-capacity")).map(
+        (element) => element.removeEventListener("click", updatePrice)
+      );
+    };
+  });
 
   const { creditAgreements, instalmentFee } = useCreditAgreements(price);
 
@@ -27,8 +72,10 @@ const CreditAgreementsProvider = ({ children }: Props) => {
       creditAgreements,
       instalmentFee,
       setPrice,
+      creditSelected,
+      setCreditSelected,
     }),
-    [creditAgreements, instalmentFee]
+    [creditAgreements, creditSelected, instalmentFee]
   );
 
   return (
